cf0fd295c5b2f056a8765e9b52c2e7f130aab1ef
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-06-14 00:02:30 +0700

    updated apidishcontroller

--- app/Http/Controllers/API/APIDishController.php
+++ app/Http/Controllers/API/APIDishController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Dish;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\DishResource;
+use App\Http\Resources\DishResources;
 
 use Illuminate\Http\Request;
 

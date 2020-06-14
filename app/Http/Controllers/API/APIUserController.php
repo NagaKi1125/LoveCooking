bc2b159930c088a8c91f05ca39db90bbea810869
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-06-14 09:33:48 +0700

    testing

--- app/Http/Controllers/API/APIUserController.php
+++ app/Http/Controllers/API/APIUserController.php
@@ -10,8 +10,8 @@ use Illuminate\Http\Request;
 class APIUserController extends Controller
 {
     public function index(){
-        $user = User::all();
-        return UserResources::collection($user);
+        return User::all();
+        //return UserResources::collection($user);
     }
 
     public function show($id)

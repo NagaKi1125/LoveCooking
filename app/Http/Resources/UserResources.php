abb0b4ee922371c7b4c8a9eb11328cf0aeadbf3b
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-06-14 10:00:03 +0700

    updated user api resources

--- app/Http/Resources/UserResources.php
+++ app/Http/Resources/UserResources.php
@@ -17,8 +17,17 @@ class UserResources extends JsonResource
      */
     public function toArray($request)
     {
-        $follow = DB::table('follows')->select('id')->where('user_id',$this->id)->get();
-
+        $follow = DB::table('follows')->select('follow_id_list')->where('user_id',$this->id)->get();
+        $foll_list = explode('_',$follow);
+        $user = User::all();
+        $follow_list = "";
+        foreach($foll_list as $foll){
+            foreach($user as $us){
+                if($foll == $us->id){
+                    $follow_list.=$us->name."_";
+                }
+            }
+        }
 
         $dish_liked = DB::table('user_liked_lists')->select('dish_id_list')->where('user_id',$this->id)->get();
         $dish_list = explode('_',$dish_liked);
@@ -42,7 +51,7 @@ class UserResources extends JsonResource
             'address'=>$this->address,
             'password'=>$this->password,
             'level'=>$this->level,
-            'follow_list_names'=>$follow,
+            'follow_list_names'=>$follow_list,
             'dish_liked_list'=>$dish_liked_list,
             'created_at'=>(String)$this->created_at,
             'updated_at'=>(String)$this->updated_at,

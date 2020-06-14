dc0760e620d7f79d038e5e8568547cd7ac24c92e
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-06-14 08:54:35 +0700

    remove passport

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'passport',
+            'driver' => '',
             'provider' => 'users',
             'hash' => false,
         ],

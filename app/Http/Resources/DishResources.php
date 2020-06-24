46ee6aa539f465d6299adde07a19ce67a81aeef2
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-06-24 20:53:56 +0700

    reroll api resources

--- app/Http/Resources/DishResources.php
+++ app/Http/Resources/DishResources.php
@@ -18,7 +18,7 @@ class DishResources extends JsonResource
     public function toArray($request)
     {
 
-        $dishhis = DB::table('dish_histories')->where('dish_id',$this->id)->pluck('dh_posts')[0];
+        $dishhis = DB::table('dish_histories')->select('dish_histories.dh_posts')->where('dish_id',$this->id)->get();
         $usercmt = DB::table('comments')
             ->join('users','comments.user_id','=','users.id')
             ->join('dishes','comments.dish_id','=','dishes.id')

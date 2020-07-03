eff8c76cd66636bc5b6ad7822f0761a9f547fead
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-07-03 17:44:47 +0700

    update menu api resources

--- app/Http/Controllers/API/APIMenuController.php
+++ app/Http/Controllers/API/APIMenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Dish;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MenuResources;
 use App\Menu;
@@ -17,19 +18,98 @@ class APIMenuController extends Controller
         return MenuResources::collection($menu);
     }
 
-    public function store(Request $req){
-        return Menu::create($req->all());
+    public function new(Request $req){
+        $user = Auth::user();
+        $params = $req->only('menu_date','breakfast_list','lunch_list','dinner_list');
+
+        $menu = new Menu();
+        $menu->user_id = $user->id;
+        $menu->menu_date = $params['menu_date'];
+        $menu->breakfast_list = $params['breakfast_list'];
+        $menu->lunch_list = $params['lunch_list'];
+        $menu->dinner_list = $params['dinner_list'];
+
+        $menu->save();
+        return response()->json($menu);
     }
 
-    public function update(Request $req,$id){
-        $menu = Menu::findOrFail($id);
-        $menu->update($req->all());
+    public function add(Request $req,$id){
+        $user = Auth::user();
+        $params = $req->only('dish_id','date_time');
+        $dish = Dish::find($params['dish_id']);
+        $menu = Menu::find($id);
+
+        if($user->id == $menu->user_id){
+            if($params['date_time']==1){
+                $menu->update([
+                    'breakfast_list'=> $menu->breakfast_list.=$dish->id."_",
+                ]);
+            }elseif($params['date_time']==2){
+                $menu->update([
+                    'lunch_list'=> $menu->lunch_list.=$dish->id."_",
+                ]);
+            }else{
+                $menu->update([
+                    'dinner_list'=> $menu->dinner_list.=$dish->id."_",
+                ]);
+            }
+        }
+
+        return response()->json($menu);
+    }
+
+    public function remove(Request $req,$id){
+        $user = Auth::user();
+        $params = $req->only('dish_id','date_time');
+        $dish = Dish::find($params['dish_id']);
+        $menu = Menu::find($id);
+
+        if($user->id == $menu->user_id){
+            if($params['date_time']==1){
+                $menuBreakfast =explode("_",$menu->breakfast_list);
+                $breakList ="";
+                foreach($menuBreakfast as $mbr){
+                    if($mbr==$dish->id){
+                        $breakList=$breakList;
+                    }else $breakList.="_".$mbr;
+                }
+                $menu->update([
+                    'breakfast_list'=> $breakList,
+                ]);
+            }elseif($params['date_time']==2){
+                $menuLunch = explode("_",$menu->lunch_list);
+                $lunchList ="";
+                foreach($menuLunch as $ml){
+                    if($ml==$dish->id){
+                        $lunchList=$lunchList;
+                    }else $lunchList.="_".$ml;
+                }
+                $menu->update([
+                    'lunch_list'=> $lunchList,
+                ]);
+            }else{
+                $menuDinner = explode("_",$menu->dinner_list);
+                $dinnerList ="";
+                foreach($menuDinner as $md){
+                    if($md==$dish->id){
+                        $dinnerList=$dinnerList;
+                    }else $dinnerList.="_".$md;
+                }
+                $menu->update([
+                    'dinner_list'=> $dinnerList,
+                ]);
+            }
+        }
+
+        return response()->json($menu);
     }
 
     public function delete($id){
 
         $menu = Menu::findOrFail($id);
         $menu->delete();
-        return 204;
+        return response()->json([
+            'delete' =>"Delete Successfully",
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,13 @@ Route::group(['middleware' => 'jwt.auth'], function () {
     Route::post('dishes/comment/{cmtid}/edit','API\APICommentController@update');
 
     Route::delete('dishes/comment/{cmtid}/delete','API\APICommentController@delete');
+
+    //Menu (make new )  - add,remove and delete
+    Route::post('menu-new','API\APIMenuController@new');
+    //?__method=PUT (@Path?__method=PUT)
+    Route::post('menu-add/{id}','API\APIMenuController@add');
+    Route::post('menu-remove/{id}','API\APIMenuController@remove');
+    Route::delete('menu-delete/{id}','API\APIMenuController@delete');
 });
 
 Route::middleware('jwt.refresh')->get('/token/refresh', 'API\APIAuthController@refresh');
@@ -51,40 +58,23 @@ Route::middleware('jwt.refresh')->get('/token/refresh', 'API\APIAuthController@r
 //categories
 Route::get('categories','API\APICategoryController@index');
 Route::get('categories/{id}','API\APICategoryController@show');
-Route::post('categories','API\APICategoryController@store');
-Route::put('categories/{id}/edit','API\APICategoryController@update');
-Route::delete('categories/{id}/delete','API\APICategoryController@destroy');
 
 //market
 Route::get('markets', 'API\APIMarketController@index');
 Route::get('markets/{id}', 'API\APIMarketController@show');
-Route::post('markets', 'API\APIMarketController@store');
-Route::put('markets/{id}', 'API\APIMarketController@update');
-Route::delete('markets/{id}', 'API\APIMarketController@delete');
 
 //dish
 Route::get('dishes', 'API\APIDishController@index');
 Route::get('dishes/{id}', 'API\APIDishController@show');
 
-//user
-Route::get('users', 'API\APIUserController@index');
-Route::post('users', 'API\APIUserController@store');
-Route::put('users/{id}', 'API\APIUserController@update');
-Route::delete('users/{id}', 'API\APIUserController@delete');
 
 //dishhistory
 Route::get('dish-history', 'API\APIDishHistoryController@index');
 Route::get('dish-history/{id}', 'API\APIDishHistoryController@show');
-Route::post('dish-history', 'API\APIDishHistoryController@store');
-Route::put('dish-history/{id}', 'API\APIDishHistoryController@update');
-Route::delete('dish-history/{id}', 'API\APIDishHistoryController@delete');
 
 //menu
 Route::get('menus', 'API\APIMenuController@index');
 Route::get('menus/{id}', 'API\APIMenuController@show');
-Route::post('menus', 'API\APIMenuController@store');
-Route::put('menus/{id}', 'API\APIMenuController@update');
-Route::delete('menus/{id}', 'API\APIMenuController@delete');
 
 
 

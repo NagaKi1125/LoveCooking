c994629de6c176284803c9d1f62541efe6a0bc51
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-07-03 17:51:26 +0700

    re-update api auth

--- app/Http/Controllers/API/APIAuthController.php
+++ app/Http/Controllers/API/APIAuthController.php
@@ -61,7 +61,7 @@ class APIAuthController extends Controller
     {
         $user = Auth::user();
         $dish_liked = DB::table('user_liked_lists')->where('user_id',$user->id)->pluck('dish_id_list')[0];
-        $follow = DB::table('follows')->where('user_id',$this->id)->pluck('follow_id_list')[0];
+        $follow = DB::table('follows')->where('user_id',$user->id)->pluck('follow_id_list')[0];
         if ($user) {
             return response([
                 "id"=> $user->id,

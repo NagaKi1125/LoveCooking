85a74a69219e5a6bf3f60b9dd0e730636411fc84
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2020-07-12 14:54:42 +0700

    api liked upgrade

---
+++ app/Http/Controllers/API/APIUserLikedListController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Dish;
+use App\Http\Controllers\Controller;
+use App\User;
+use App\UserLikedList;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class APIUserLikedListController extends Controller
+{
+    public function love($id){
+        $user = Auth::user();
+        $userLoveDish = UserLikedList::all();
+        $dish = Dish::find($id);
+        $dishlike = $dish->liked_count;
+
+        foreach($userLoveDish as $loveDish){
+            if($loveDish->user_id == $user->id){
+                $loveID = $loveDish->id;
+                $lovedish = $loveDish->dish_id_list;
+            }
+        }
+
+        $UserLikedDish = UserLikedList::find($loveID);
+
+        if(strpos($lovedish,$id) != false){
+
+        }else{
+            $like = $dish->liked_count;
+            $dish->update([
+                'liked_count' => $like+1,
+            ]);
+
+            $UserLikedDish->update([
+                'dish_id_list'=>$lovedish.=$id."_",
+            ]);
+        }
+
+        return response()->json($UserLikedDish);
+    }
+
+    public function loveless($id){
+        $user = Auth::user();
+        $userLoveDish = UserLikedList::all();
+        $dish = Dish::find($id);
+
+        foreach($userLoveDish as $loveDish){
+            if($loveDish->user_id == $user->id){
+                $loveID = $loveDish->id;
+                $lovedish = $loveDish->dish_id_list;
+            }
+        }
+
+        $UserLikedDish = UserLikedList::find($loveID);
+        if(strpos($lovedish,$id) != false){
+            $like = $dish->liked_count;
+            $dish->update([
+                'liked_count' => $like-1,
+            ]);
+            $lovelist = str_replace($id."_","",$lovedish);
+            $UserLikedDish->update([
+                'dish_id_list'=>$lovelist,
+            ]);
+        }
+
+        return response()->json($UserLikedDish);
+    }
+}
